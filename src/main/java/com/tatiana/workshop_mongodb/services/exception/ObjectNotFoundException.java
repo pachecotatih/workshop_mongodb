64f9104801a74c5ed08c7c3d6e92e66c73831221
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-23 11:44:51 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/tatiana/workshop_mongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.tatiana.workshop_mongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
