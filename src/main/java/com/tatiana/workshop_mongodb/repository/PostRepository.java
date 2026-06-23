cb17292c708bf29590136578af40c8d5798b4957
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-23 13:44:34 -0300

    Consulta com vários critérios

--- src/main/java/com/tatiana/workshop_mongodb/repository/PostRepository.java
+++ src/main/java/com/tatiana/workshop_mongodb/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.tatiana.workshop_mongodb.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,6 +14,11 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
+    // Do mongoDB query
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Post> findByTitle(String text);
+
+    // Do mongoDB query
+    @Query("{$and: [{date: {$gte: ?1}}, {date: {$lte: ?2}}, {$or: [{'title': { $regex: ?0, $options: 'i' }},{'body': { $regex: ?0, $options: 'i' }}, {'comments.text': { $regex: ?0, $options: 'i' }}]}]}")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/tatiana/workshop_mongodb/services/PostService.java
+++ src/main/java/com/tatiana/workshop_mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.tatiana.workshop_mongodb.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,10 @@ public class PostService {
     public List<Post> findByTitle(String text) {
         List<Post> list = repo.findByTitle(text);
         return list;
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); // 24 horas
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
